ffe3db4cdaa7a534a707e3d77eb880b9bed9b113
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-01-12 18:36:38 -0500

    Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :user_id,
+            presence: true
+
+  validates :title,
+            presence: true,
+            uniqueness: true
+
+  validates :body,
+            presence: true
 end
